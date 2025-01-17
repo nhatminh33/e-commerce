781dddb8607267ce048d026a84ee1ccff2f05a99
nhatminh33 <minh5500vn@gmail.com>
2025-01-17 21:12:51 +0700

    up

--- dashboard/src/navigation/allNav.js
+++ dashboard/src/navigation/allNav.js
@@ -8,56 +8,56 @@ import { IoIosChatbubbles } from "react-icons/io";
 export const allNav = [
     {
         id : 1,
-        title : 'Dashboard',
+        title : 'Dashboard (Bảng điều khiển)',
         icon : <AiOutlineDashboard />,
         role : 'admin',
         path: '/admin/dashboard'
     },
     {
         id : 2,
-        title : 'Orders',
+        title : 'Orders (Đơn hàng)',
         icon : <AiOutlineShoppingCart />,
         role : 'admin',
         path: '/admin/dashboard/orders'
     },
     {
         id : 3,
-        title : 'Category',
+        title : 'Category (Danh mục)',
         icon : <BiCategory  />,
         role : 'admin',
         path: '/admin/dashboard/category'
     },
     {
         id : 4,
-        title : 'Sellers',
+        title : 'Sellers (Người bán)',
         icon : <FaUsers   />,
         role : 'admin',
         path: '/admin/dashboard/sellers'
     },
     {
         id : 5,
-        title : 'Payment Request',
+        title : 'Payment Request (Yêu cầu thanh toán)',
         icon : <MdPayment />,
         role : 'admin',
         path: '/admin/dashboard/payment-request'
     },
     {
         id : 6,
-        title : 'Deactive Sellers',
+        title : 'Deactive Sellers (Người bán bị vô hiệu)',
         icon : <FaUserTimes />,
         role : 'admin',
         path: '/admin/dashboard/deactive-sellers'
     },
     {
         id : 7,
-        title : 'Seller Request',
+        title : 'Seller Request (Yêu cầu đăng ký bán)',
         icon : <FaCodePullRequest />,
         role : 'admin',
         path: '/admin/dashboard/sellers-request'
     },
     {
         id : 8,
-        title : 'Live Chat',
+        title : 'Live Chat (Trò chuyện trực tuyến)',
         icon : <IoIosChatbubbles />,
         role : 'admin',
         path: '/admin/dashboard/chat-seller'
